94b875322b0525c2a6dd8287bb182c7eeb8f405d
vladaliho <vlada.liho@gmail.com>
2024-09-18 14:01:16 +0100

    feat: add prompt message

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -1,3 +1,3 @@
 const message = prompt(
-	'Привет! Вместо ссылки на пул реквест залила неправильную ссылку на деплой! Вот ссылка на пул реквест: https://github.com/vladaliho/cssMemeSlider/pull/2'
+	'Привет! Вместо ссылки на пул реквест залила неправильную ссылку на деплой! Вот ссылка на пул реквест: https://github.com/vladaliho/cssMemeSlider/pull/4'
 );
